b5d74bebd0adf1891e73bde82e02dc18f6749900
duqiao <duqiao_1986@163.com>
2015-08-12 13:19:54 +0800

    maven project

---
+++ phenix.config/src/main/java/qiaodu/phenix/config/App.java
@@ -0,0 +1,13 @@
+package qiaodu.phenix.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
